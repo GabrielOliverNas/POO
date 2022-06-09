b2a98433c5986675c1374cd6d405dd26f5411f00
Gabriel de Oliveira <Gabriel.nascimento@deal.com>
2022-06-09 14:06:54 -0300

    Encontrando valor de KM/h a partir da distancia e o tempo

---
+++ JavaApplication20/src/SpringBootApplication.java
@@ -0,0 +1,38 @@
+import static java.lang.Double.valueOf;
+import static java.lang.String.format;
+
+public class SpringBootApplication {
+
+  private static final Integer VALOR_HORA = 60;
+  private static final Integer VALOR_KM = 1000;
+
+  public static void main(String[] args) {
+    var distanciaPercorridaMetro = 4.0;
+    var tempoAcionamentoMin = valueOf(1);
+
+    final Double velocidadeKm =
+        calcularVelocidadeMin(distanciaPercorridaMetro, tempoAcionamentoMin);
+
+    System.out.println(format("A velocidade atual é de %.2f", velocidadeKm) + "Km/H");
+  }
+
+  private static Double calcularVelocidadeMin(
+      Double distanciaPercorridaMetro, Double tempoAcionamentoMin) {
+    var tempoAcionamentoHoras = minutosParaHora(tempoAcionamentoMin);
+
+    return calcularVelocidadeKm(
+        calcularKmPorMetros(distanciaPercorridaMetro), tempoAcionamentoHoras);
+  }
+
+  private static Double minutosParaHora(Double tempoAcionamentoMin) {
+    return tempoAcionamentoMin / VALOR_HORA;
+  }
+
+  private static Double calcularVelocidadeKm(Double kmMetro, Double tempoAcionamentoHoras) {
+    return kmMetro / tempoAcionamentoHoras;
+  }
+
+  private static Double calcularKmPorMetros(Double metros) {
+    return metros / VALOR_KM;
+  }
+}
